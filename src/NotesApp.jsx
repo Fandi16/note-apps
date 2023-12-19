be8901315f1b36ab23236526abfe87ee7e9c5f59
Fandi16 <fanditok16@gmail.com>
2023-12-20 06:53:58 +0700

    update localstorage

--- src/NotesApp.jsx
+++ src/NotesApp.jsx
@@ -3,60 +3,73 @@ import NotesList from "./NotesList";
 import NoteInput from "./components/NotesInput";
 import { getInitialData } from "./utils";
 
-class NoteApp extends React.Component
-{
-    constructor(props){
-        super( props );
-        this.state = {
-            Notes: getInitialData(),
-        }
+class NoteApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Notes: getInitialData(),
+    };
 
-        this.onDeleteHendler = this.onDeleteHendler.bind( this );
-        this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
-    }
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
+  }
 
-    onDeleteHendler ( id )
-    {
-        const Notes = this.state.Notes.filter( Notes => Notes.id !== id );
-        this.setState( { Notes } );
+  componentDidMount() {
+    // Memuat catatan dari localStorage saat komponen dimount
+    const localNotes = JSON.parse(localStorage.getItem("notes")) || [];
+    this.setState({
+      Notes: localNotes,
+    });
+  }
 
-    }
+  onDeleteHandler(id) {
+  const updatedNotes = this.state.Notes.filter((note) => note.id !== id);
+  this.setState({ Notes: updatedNotes });
+  // Menyimpan catatan ke localStorage setelah menghapus
+  localStorage.setItem("notes", JSON.stringify(updatedNotes));
+}
+
+
     onAddNoteHandler ( { title, body } )
     {
     const date = new Date();
-    this.setState((prevState) => {
+    this.setState(
+      (prevState) => {
         return {
-        Notes: [
+          Notes: [
             ...prevState.Notes,
             {
-            id: +new Date(),
-            title,
-            body,
-            createdAt: date.toISOString(),
-            archived: false,
-            }
-        ]
-        }
-    });
-    }
+              id: +new Date(),
+              title,
+              body,
+              createdAt: date.toISOString(),
+              archived: false,
+            },
+          ],
+        };
+      },
+      () => {
+        // Menyimpan catatan ke localStorage setelah menambahkan
+        localStorage.setItem("notes", JSON.stringify(this.state.Notes));
+      }
+    );
+  }
 
-    render ()
-    {
-        return (
-            <div className="note-app">
-                <h2>Tambah Kontak</h2>
-                <NoteInput addNote={this.onAddNoteHandler} />
-                <h1>  Daftar Catatan </h1>
-               
-                {this.state.Notes.length > 0 ? (
-                    <NotesList Notes={this.state.Notes} onDelete={this.onDeleteHendler} />
-                ) : (
-                    <p className="noting">Tidak ada catatan</p>
-                ) }
-                
-            </div> 
-        )
-    }
+  render() {
+    return (
+      <div className="note-app">
+        <h2>Tambah Catatan</h2>
+        <NoteInput addNote={this.onAddNoteHandler} />
+        <h1>Daftar Catatan</h1>
+
+        {this.state.Notes.length > 0 ? (
+          <NotesList Notes={this.state.Notes} onDelete={this.onDeleteHandler} />
+        ) : (
+          <p className="noting">Tidak ada catatan</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default NoteApp;
--- src/components/NotesInput.jsx
+++ src/components/NotesInput.jsx
@@ -1,55 +1,63 @@
 import React from 'react';
-    
-class NoteInput extends React.Component
-{
-    constructor(props) {
+
+class NoteInput extends React.Component {
+  constructor(props) {
     super(props);
     // inisialisasi state
     this.state = {
-        title: '',
-        body: '',
-    }
-    
+      title: '',
+      body: '',
+    };
+
     this.ontitleChangeEventHandler = this.ontitleChangeEventHandler.bind(this);
     this.onbodyChangeEventHandler = this.onbodyChangeEventHandler.bind(this);
     this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
-    }
-    
-    ontitleChangeEventHandler(event) {
-    this.setState(() => {
-        return {
-        title: event.target.value,
-        }
-    });
-    }
-    
-    onbodyChangeEventHandler(event) {
-    this.setState(() => {
-        return {
-        body: event.target.value,
-        }
-    });
-    }
-    
-    onSubmitEventHandler(event) {
-    event.preventDefault();
-    this.props.addNote(this.state);
+  }
 
+  ontitleChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        title: event.target.value,
+      };
+    });
+  }
+
+  onbodyChangeEventHandler(event) {
+    this.setState(() => {
+      return {
+        body: event.target.value,
+      };
+    });
+  }
+
+  onSubmitEventHandler(event) {
+    event.preventDefault();
+    
+    // Menyimpan data ke local storage
+    const note = { title: this.state.title, body: this.state.body };
+    const existingNotes = JSON.parse(localStorage.getItem('notes')) || [];
+    const newNotes = [...existingNotes, note];
+    localStorage.setItem('notes', JSON.stringify(newNotes));
+
+    // Menyampaikan data ke parent component (jika diperlukan)
+    this.props.addNote && this.props.addNote(this.state);
+
+    // Mengosongkan state setelah menyimpan
     this.setState({
       title: '',
       body: '',
     });
-    }
+  }
 
-    render() {
+  render() {
     return (
-        <form className='Note-input' onSubmit={this.onSubmitEventHandler}>
-            <input type="text" placeholder="title" value={ this.state.title } onChange={ this.ontitleChangeEventHandler } />
-            <textarea cols="60" rows="10" type="text" placeholder="body" value={this.state.body} onChange={this.onbodyChangeEventHandler}></textarea>
-            <button type="submit">Tambah</button>
-        </form>
-    )
-    }
+      <form className='Note-input' onSubmit={this.onSubmitEventHandler}>
+        <input type="text" placeholder="title" value={this.state.title} onChange={this.ontitleChangeEventHandler} />
+        <textarea cols="60" rows="10" type="text" placeholder="body" value={this.state.body} onChange={this.onbodyChangeEventHandler}></textarea>
+        <button type="submit">Tambah</button>
+      </form>
+    );
+  }
 }
-    
+
 export default NoteInput;
